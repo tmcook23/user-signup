b51e49e2f63d5120c29a608650f3614219d3901e
Tracy Cook <tracymcook23@gmail.com>
2019-04-08 19:33:23 -0500

    Include error classes for the four input fields

--- main.py
+++ main.py
@@ -1,32 +1,38 @@
-from flask import Flask, request
+from flask import Flask, request, redirect
+import cgi
 
 app = Flask(__name__)
-app .config['DEBUG'] = True
-
+app.config['DEBUG'] = True
 
 form = """
-
 <!doctype html>
 <html>
+    <style>
+        .error {{ color: red; }}
+    </style>
     <head>
         <h1>Signup</h1>
     </head>
     <body>
-        <form>
+        <form method="POST">
             <label for= "username">Username</label>
-            <input id="username" type = "text" />
+            <input name="hours" id="username" type="text" value='{username}' />
+            <p class = "error">{username_error}</p>
             <br>
             <br>
             <label for= "password">Password</label>
-            <input id="password" type="password" />
+            <input name="password" id="password" type="password" value='{password}' />
+            <p class = "error">{password_error}</p>
             <br>
             <br>
             <label for= "verify_password">Verify Password</label>
-            <input id="verify_password" type="password" />
+            <input name="verify_password" id="verify_password" type="password" value='{verify_password}' />
+            <p class = "error">{verify_password_error}</p>
             <br>
             <br>
             <label for= "email">Email (optional)</label>
-            <input id="email" type="text"/>
+            <input name="email" id="email" type="text" value='{email}' />
+            <p class = "error">{email_error}</p>
             <br>
             <br>
             <input type="submit" /> 
@@ -43,11 +49,17 @@ form = """
 
 @app.route("/")
 def home():
-    return form
+    return form.format(username='', username_error='', password='', password_error='', verify_password='', verify_password_error='', email='', email_error='')
 
-@app.route("/welcome", methods=['POST'])
+
+@app.route("/welcome")
 def welcome():
     username = request.form['username']
     return '<h1>Welcome, ' + username + '!</h1>'
+
+
+
+
+
 
 app.run()
