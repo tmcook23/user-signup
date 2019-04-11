0cbd7ef6ed7a3e39444f33f6bdb3f3afbded4ac0
Tracy Cook <tracymcook23@gmail.com>
2019-04-10 19:35:04 -0500

    Checks sign-up form for user-submitted errors.

--- main.py
+++ main.py
@@ -62,32 +62,37 @@ def validate_signup():
     if username == '':
         username_error = 'Enter a username'
         username = username
-    #elif username #CONTAINS A SPACE:
-    #    username_error = 'Usernames cannot contain spaces. Enter a valid username.'
-    #    username = username
+    elif ' ' in username: # IF USERNAME CONTAINS A SPACE:
+        username_error = 'Usernames cannot contain spaces. Enter a valid username.'
+        username = username
     elif len(username) < 3 or len(username) > 20:
         username_error = 'Enter a valid username (3-20 characters).'
         username = username
     
     if password == '':
         password_error = 'Enter a password'
-    #elif password #CONTAINS A SPACE:
-    #    password_error = 'Passwords cannot contain spaces. Enter a valid password.'
+    elif ' ' in password: # IF PASSWORD CONTAINS A SPACE:
+        password_error = 'Passwords cannot contain spaces. Enter a valid password.'
     elif len(password) < 3 or len(password) > 20:
         password_error = 'Enter a valid password (3-20 characters).'
     
     if verify_password != password:
         verify_password_error = 'Does not match password.'
 
-    #if email != '':
-        # but if there is content in it, then it must be validated. The criteria for a valid email address in this assignment are that it has a single @, a single ., contains no spaces, and is between 3 and 20 characters long.
-        # MAKE EMAIL = EMAIL
+    if email != '': # Email is optional, but if there is content in it, then it must be validated.
+        if len(email) < 3 or len(email) > 20:
+            email_error = 'Enter a valid email (3-20 characters).'
+            email = email
+        elif '@' and '.' not in email:
+            email_error = 'Enter a valid email.'
+            email = email
+        # The criteria for a valid email address in this assignment are that it has a single @, a single ., contains no spaces, and is between 3 and 20 characters long.
 
-    if not username_error and not password_error and not verify_password_error: #AND NOT EMAIL_ERROR
+    if not username_error and not password_error and not verify_password_error and not email_error:
         username = username
         return redirect('/welcome?username={0}'.format(username))
     else:
-        return form.format(username_error=username_error, password_error=password_error) #hours=hours, minutes=minutes)
+        return form.format(username_error=username_error, password_error=password_error, verify_password_error=verify_password_error, email_error=email_error, username=username, password=password, verify_password=verify_password, email=email) # FROM EXAMPLE: hours=hours, minutes=minutes
 
 
 # Creating an HTTP Redirect (must import redirect):
