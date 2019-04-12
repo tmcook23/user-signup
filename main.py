7eedef4421940ce497dfe7fb0b247dd85e2fbc83
Tracy Cook <tracymcook23@gmail.com>
2019-04-11 19:02:02 -0500

    Make use of templates.

--- main.py
+++ main.py
@@ -1,51 +1,14 @@
-from flask import Flask, request, redirect
+from flask import Flask, request, redirect, render_template
 import cgi
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-form = """
-<!doctype html>
-<html>
-    <style>
-        .error {{ color: red; }}
-    </style>
-    <head>
-        <h1>Signup</h1>
-    </head>
-    <body>
-        <form method="POST">
-            <label for= "username">Username</label>
-            <input name="username" id="username" type="text" value='{username}' /> <span class = "error">{username_error}</span>
-            <br>
-            <br>
-            <label for= "password">Password</label>
-            <input name="password" id="password" type="password" /> <span class = "error">{password_error}</span>
-            <br>
-            <br>
-            <label for= "verify_password">Verify Password</label>
-            <input name="verify_password" id="verify_password" type="password" /> <span class = "error">{verify_password_error}</span>
-            <br>
-            <br>
-            <label for= "email">Email (optional)</label>
-            <input name="email" id="email" type="text" value='{email}' /> <span class = "error">{email_error}</span>
-            <br>
-            <br>
-            <input type="submit" /> 
-        </form>
-    </body>
-    <footer>
-        <p>Form created by: Tracy Cook</p>
-        <p>April 7, 2019</p>
-        <p>LC101 User-Signup</p>
-    </footer>
-</html>
 
-"""
 
 @app.route("/")
 def home():
-    return form.format(username='', username_error='', password='', password_error='', verify_password='', verify_password_error='', email='', email_error='')
+    return render_template('home.html')
 
 @app.route("/", methods=['POST'])
 def validate_signup():
@@ -60,7 +23,7 @@ def validate_signup():
     email_error = ''
 
     if username == '':
-        username_error = 'Enter a username'
+        username_error = 'Enter a username.'
         username = username
     elif ' ' in username: # IF USERNAME CONTAINS A SPACE:
         username_error = 'Usernames cannot contain spaces. Enter a valid username.'
@@ -70,7 +33,7 @@ def validate_signup():
         username = username
     
     if password == '':
-        password_error = 'Enter a password'
+        password_error = 'Enter a password.'
     elif ' ' in password: # IF PASSWORD CONTAINS A SPACE:
         password_error = 'Passwords cannot contain spaces. Enter a valid password.'
     elif len(password) < 3 or len(password) > 20:
@@ -93,18 +56,19 @@ def validate_signup():
 
     if not username_error and not password_error and not verify_password_error and not email_error:
         username = username
-        return redirect('/welcome?username={0}'.format(username))
+        return render_template('welcome.html', username=username)
+        #return redirect('/welcome?username={0}'.format(username))
     else:
-        return form.format(username_error=username_error, password_error=password_error, verify_password_error=verify_password_error, email_error=email_error, username=username, password=password, verify_password=verify_password, email=email) #FROM EXAMPLE: hours=hours, minutes=minutes, etc.
-
+        return render_template('home.html', username_error=username_error, password_error=password_error, verify_password_error=verify_password_error, email_error=email_error, username=username, password=password, verify_password=verify_password, email=email)
 
 # Creating an HTTP Redirect (must import redirect):
 
 @app.route("/welcome")
 def welcome():
-    #username = request.form['username'] #request.form gets stuff from the body of the request (post request)
+    username = request.form['username'] #request.form gets stuff from the body of the request (post request)
     #return '<h1>Welcome, ' + username + '!</h1>'
-    username = request.args.get('username') #args.get gets stuff from query parameter
-    return '<h1>Welcome, {0}!</h1>'.format(username)
+    #username = request.args.get('username') #args.get gets stuff from query parameter
+    #return .format(username)
+    return render_template('welcome.html', username=username)
 
 app.run()
